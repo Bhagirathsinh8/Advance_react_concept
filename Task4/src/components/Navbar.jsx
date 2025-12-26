5728a85a491c36a95b901e4da4b8f23087beefaa
Bhagirathsinh8 <bhagirathnakum8@gmail.com>
2025-12-26 13:59:59 +0530

    Task 4 Done

---
+++ Task4/src/components/Navbar.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import logo from "../assets/logo123.png";
+export default function Navbar() {
+  return (
+    <div>
+      <nav class="relative bg-gray-800/20 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div class="relative flex h-16 items-center justify-between">
+            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              {/* <!-- Mobile menu button--> */}
+              <button
+                type="button"
+                command="--toggle"
+                commandfor="mobile-menu"
+                class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500"
+              >
+                <span class="absolute -inset-0.5"></span>
+                <span class="sr-only">Open main menu</span>
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  data-slot="icon"
+                  aria-hidden="true"
+                  class="size-6 in-aria-expanded:hidden"
+                >
+                  <path
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  data-slot="icon"
+                  aria-hidden="true"
+                  class="size-6 not-in-aria-expanded:hidden"
+                >
+                  <path
+                    d="M6 18 18 6M6 6l12 12"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div class="flex shrink-0 items-center">
+                <img
+                  src={logo}
+                  alt="Your Company"
+                  class="h-10 w-24 object-contain "
+                />
+              </div>
+              <div class="hidden sm:ml-6 sm:block">
+                <div class="flex space-x-4">
+                  {/* <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" --> */}
+                  <a
+                    href="#"
+                    aria-current="page"
+                    class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="#"
+                    aria-current="page"
+                    class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white"
+                  >
+                    Products
+                  </a>
+                  <a
+                    href="#"
+                    aria-current="page"
+                    class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white"
+                  >
+                    About US
+                  </a>
+                </div>
+              </div>
+            </div>
+         
+          </div>
+        </div>
+
+        <el-disclosure id="mobile-menu" hidden class="block sm:hidden">
+          <div class="space-y-1 px-2 pt-2 pb-3">
+            {/* <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" --> */}
+            <a
+              href="#"
+              aria-current="page"
+              class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white"
+            >
+              Dashboard
+            </a>
+         
+          </div>
+        </el-disclosure>
+      </nav>
+    </div>
+  );
+}
