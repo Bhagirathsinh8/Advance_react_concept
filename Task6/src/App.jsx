7a36a77f9d2a92462c635a1e00c4ac7a575363d5
Bhagirathsinh8 <bhagirathnakum8@gmail.com>
2025-12-28 20:01:46 +0530

    Task-6 Done
    Using UseFormik Hook and Formik Tag

---
+++ Task6/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import MultiStepForm from './componets/MultiStepForm'
+
+export default function App() {
+  return (
+    <div>
+      {/* <TempForm/> */}
+      {/* <SignupForm/> */}
+
+      <MultiStepForm/>
+    </div>
+  )
+}
